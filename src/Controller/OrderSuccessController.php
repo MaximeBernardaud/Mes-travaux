38bda1034ca575e1757a1de96ec2aa638561ab5e
maxime Bernardaud <maxime.bernardaud@gmail.com>
2021-03-22 12:08:57 +0100

    Version finale cour symfony

---
+++ src/Controller/OrderSuccessController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderValidateController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    #[Route('/commande/merci/{stripeSessionId}', name: 'order_validate')]
+    public function index($stripeSessionId): Response
+    {
+        $order = $this->entityManager->getRepository(Order::class)->findOneByStripeSessionId($stripeSessionId);
+
+        if(!$order || $order->getUser() != $this->getUser()){
+            return $this->redirectToRoute('home');
+        }
+        if(!$order->getIsPaid()){
+            //modifier le statut isPaid de notre commande en mettant 1:
+            $order->setIsPaid(1);
+            $this->entityManager->flush();
+
+            //envoyer un email à notre client pour lui confirmer sa commande
+        }
+
+
+        return $this->render('order_validate/index.html.twig', [
+            'order' =>$order
+        ]);
+    }
+}
